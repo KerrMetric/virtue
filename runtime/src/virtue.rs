b613053f1901737f72fe9d6690082ef1dd35b61f
shiki <shiki.tak@gmail.com>
2019-08-04 18:05:24 +0900

    add send_transaction

--- runtime/src/virtue.rs
+++ runtime/src/virtue.rs
@@ -1,4 +1,5 @@
-use support::{decl_storage, decl_module};
+use support::{decl_storage, decl_module, StorageValue, dispatch::Result};
+use system::ensure_signed;
 
 pub trait Trait: system::Trait {}
 
@@ -11,5 +12,11 @@ decl_storage! {
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         // Declare public functions here
+
+        fn send_transaction(origin, input_bool: bool) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            Ok(())
+        }
     }
 }
