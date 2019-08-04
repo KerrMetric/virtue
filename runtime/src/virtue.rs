c92a4eb3a7b8fb66c423f7c9485a9f4bd0e3b134
shiki <shiki.tak@gmail.com>
2019-08-04 18:15:19 +0900

    add transaction struct

--- runtime/src/virtue.rs
+++ runtime/src/virtue.rs
@@ -3,6 +3,11 @@ use system::ensure_signed;
 
 pub trait Trait: system::Trait {}
 
+pub struct Transaction<AccountId> {
+    from: AccountId,
+    to: AccountId,
+}
+
 decl_storage! {
     trait Store for Module<T: Trait> as VirtueStorage {
         // Declare storage and getter functions here
@@ -13,8 +18,10 @@ decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         // Declare public functions here
 
-        fn send_transaction(origin, input_bool: bool) -> Result {
+        fn send_transaction(origin, to: T::AccountId) -> Result {
             let sender = ensure_signed(origin)?;
+
+            let tx = Transaction { from: sender, to: to };
 
             Ok(())
         }
